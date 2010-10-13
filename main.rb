ce5b733a9c741b780d698fc862f6072d7a0b9858
eggXILE <blastdozer@gmail.com>
2010-10-13 18:29:08 +0900

    Renewed all
     Added parse() function.
     Rewrote main.rb and README.

--- main.rb
+++ main.rb
@@ -1,38 +1,26 @@
-require "open-uri"
+require 'get_sale'
 
-TITLE_PATTERN = /^<title>(.*)\son\sSteam<\/title>$/
-PRICE_PATTERN = /^.*<strike>&#36;([0-9]+\.[0-9]+)<\/strike><\/span>\s&#36;([0-9]+\.[0-9]+)<\/div>$/
+result_file = open('result.txt', 'w')
 
-def parseUri(uri)
-  open(uri){|f|
-    while line = f.gets
-      line.chomp!
-      if TITLE_PATTERN =~ line then
-        print "<a href=\"#{uri}\" target=\"_blank\">#{$1}</a> "
-      elsif PRICE_PATTERN =~ line then
-        print "$#{$1} -&gt; $#{$2} "
-        printf("(%2d%%off)\n", (1.00 - $2.to_f / $1.to_f) * 100)
+begin
+  url_file = open(filename = (ARGV[0] || "url.txt"))
+rescue
+  print 'Can\'t open ' + filename + "\n"
+  print 'Usage: ' + __FILE__ + " <filename>\n"
+  exit!
+else
+  while line = url_file.gets
+    if !line.chomp!.empty?
+      game = parse(line)
+      if game[:discount?]
+        result_file.print '<a href="' + game[:url] + '" target="_blank">' + game[:title] + '</a> $' \
+          + game[:original].to_s + ' -&gt; $' + game[:final].to_s + ' (' + game[:pct].to_s + "% off)\n"
       end
-    end
-  }
+      else
+        next
+      end
+  end
+  url_file.close
 end
 
-mode = {}
-if ARGV[0] =~ /^-u$/ then
-  mode["type"] = "uri"
-  mode["uri"] = ARGV[1]
-elsif ARGV[0] =~ /^-f$/ then
-  mode["type"] = "file"
-  mode["file"] = ARGV[1]
-end
-
-if mode["type"] == "uri" then
-  parseUri(mode["uri"])
-elsif mode["type"] == "file" then
-  open(mode["file"]){|f|
-    while line = f.gets
-      line.chomp!
-      parseUri(line)
-    end
-  }
-end
+result_file.close
